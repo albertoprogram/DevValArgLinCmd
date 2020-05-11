156f1a322fe514567dc5de91320de18d1f3048c4
Alberto Chirinos <developernet@outlook.com>
2020-05-11 17:32:02 -0500

    Se programa retorno de valor 1 ó 0 dependiendo del valor del argumento de entrada al iniciar el programa.

---
+++ DevValArgLinCmd/DevValArgLinCmd/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevValArgLinCmd
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            if (args[0] == "Falla")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
